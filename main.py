da2a3221528b2dba1afa626f0e86a8d878b78b26
Daniel-Naumoski <daniel.naumoski@gmail.com>
2023-06-08 14:48:10 +0200

    Main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import swap
+import extras.desc
+
+print("Please enter two numbers to swap to string: ")
+input1=()
+input2=()
+print(swap_function(input1, input2))
+
+print("Please enter a number to print it in natural descending order: ")
+input3=()
+print(desc_order(input3))
