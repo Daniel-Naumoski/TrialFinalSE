581ad2561d47c2fc80373da382f0fc5ad2de5c30
Daniel-Naumoski <daniel.naumoski@gmail.com>
2023-06-08 14:55:47 +0200

    Merge1?

---
+++ extras/desc.py
@@ -0,0 +1,10 @@
+def desc_order(number):
+    print(number)
+    while(number>1):
+        number=number-1
+        print(number)
+        
+
+
+print(desc_order(5))
+
---
+++ swap.py
@@ -0,0 +1,20 @@
+def swap_function(number1, number2):
+    if(number1==0):
+        string1="zero"
+    if(number1==1):
+        string1="one"
+    if(number1==2):
+        string1="two"
+    if(number1==3):
+        string1="three"
+
+    if(number2==0):
+        string2="zero"
+    if(number2==1):
+        string2="one"
+    if(number2==2):
+        string2="two"
+    if(number2==3):
+        string2="three"
+    return string1, string2
+
